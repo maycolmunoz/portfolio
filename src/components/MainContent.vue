4d8607c7412053d52e5df71a8d070e7ac00fa66b
maycolmunoz <estivenyeizon@gmail.com>
2025-11-13 21:25:12 -0500

    refactor: 💡 theme and update portfolio

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -9,7 +9,14 @@ import ButtonExperience from './buttons/ButtonExperience.vue'
 <template>
   <main class="grid grid-cols-1 gap-6 md:gap-10C md:my-10 md:grid-cols-2 items-center">
     <article class="space-y-6 px-6 md:px-10">
-      <Badge class="bg-emerald-500 py-2 px-4 text-base md:text-xl font-extrabold" as="h1"> $Backend Developer </Badge>
+      <div class="rounded-lg bg-gradient-to-r from-indigo-950 to-sky-950 animate-pulse text-center">
+        <Badge
+          class="bg-secondary py-2 px-4 text-base md:text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-sky-500"
+          as="h1"
+        >
+          Backend Developer
+        </Badge>
+      </div>
 
       <p class="mt-2 text-slate-300 text-base md:text-xl leading-relaxed">
         My name is <span class="font-semibold text-white">Maycol Muñoz</span>, a Systems Engineering student focused on backend development.
@@ -17,7 +24,11 @@ import ButtonExperience from './buttons/ButtonExperience.vue'
       </p>
 
       <div class="flex flex-wrap gap-2 mt-3">
-        <Badge v-for="tech in techStack" :key="tech.name" :class="`px-3 py-1 text-sm font-semibold ${tech.color}`">
+        <Badge
+          v-for="tech in techStack"
+          :key="tech.name"
+          :class="`px-3 text-sm font-semibold ${tech.color} hover:scale-110 cursor-default`"
+        >
           > {{ tech.name }}
         </Badge>
       </div>
@@ -40,14 +51,7 @@ import ButtonExperience from './buttons/ButtonExperience.vue'
     </article>
 
     <article class="flex justify-center items-center px-4">
-      <lord-icon src="https://cdn.lordicon.com/anzrsjfm.json" trigger="hover" style="width: 400px; height: 400px"> </lord-icon>
+      <img alt="Avatar" width="400px" class="hover:animate-pulse" src="https://api.dicebear.com/9.x/bottts/svg?seed=m" />
     </article>
-
-    <footer class="text-center text-xs text-slate-500">
-      Icons and animations by
-      <a href="https://lordicon.com/" target="_blank" rel="noopener" class="underline">Lordicon</a>
-      and
-      <a href="https://cocomaterial.com/" target="_blank" rel="noopener" class="underline">Cocomaterial</a>.
-    </footer>
   </main>
 </template>
--- src/components/icons/IconGitHub.vue
+++ src/components/icons/IconGitHub.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="30"
+    height="30"
     viewBox="0 0 24 24"
     fill="currentColor"
     class="icon icon-tabler icons-tabler-filled icon-tabler-brand-github"
